6397a85971b163f392b7b4158fd5e12acbab9d8a
wolkins <h.fukuhara0606@gmail.com>
2022-03-02 01:21:50 +0900

    model: validation 作成

--- app/models/win_history_number.rb
+++ app/models/win_history_number.rb
@@ -1,3 +1,12 @@
 class WinHistoryNumber < ApplicationRecord
   belongs_to :win_history, inverse_of: :win_history_numbers
+
+  validate :win_history_numbers_count_must_be_within_limit
+
+  private
+
+  def win_history_numbers_count_must_be_within_limit
+    errors.add(:base, :posts_count_must_be_within_limit) if
+      win_history.win_history_numbers.count >= win_history.loto_type.history_number_count
+  end
 end
